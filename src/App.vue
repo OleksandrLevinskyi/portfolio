b64cef235401ef1d7dbce5e64e30110d5475ddb0
OleksandrLevinskyi <aleksandrlevinskyi@gmail.com>
2022-06-16 01:21:59 -0400

    add particle background

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,43 @@
 <template>
   <div id="app">
-    Oleksandr Levinskyi
+    <h1>Oleksandr Levinskyi</h1>
+    <div id="particles-js"></div>
   </div>
 </template>
 
 <script>
+import "particles.js";
+import {particlesConfig} from "./particles-config";
+
 export default {
   name: 'App',
-  components: {}
+  components: {},
+  mounted() {
+    window.particlesJS("particles-js", particlesConfig);
+  }
 }
 </script>
 
-<style></style>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300&display=swap');
+
+body {
+  margin: 0;
+  font-family: 'Space Grotesk', sans-serif;
+}
+
+canvas {
+  display: block;
+  vertical-align: bottom;
+}
+
+#particles-js {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: 50% 50%;
+}
+</style>
