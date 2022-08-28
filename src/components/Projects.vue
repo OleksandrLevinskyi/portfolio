62158b6da12428c59e526fda330b0a1babc43dac
OleksandrLevinskyi <aleksandrlevinskyi@gmail.com>
2022-08-27 21:10:54 -0400

    finish designing projects component for desktops

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,31 +1,35 @@
 <template>
-  <div class="d-flex flex-column align-content-center p-3">
+  <div class="d-flex flex-column flex-md-row p-3 p-sm-5 mx-lg-5">
     <iframe class="video"
             src="https://www.youtube.com/embed/6fWU0e6W8QY"
             title="YouTube project demo"
             allowfullscreen
     ></iframe>
 
-    <h1>Project Title
-      <a href="https://github.com/OleksandrLevinskyi"
-         class="ms-3"
-         target=”_blank”>
-        <b-icon-github class="fs-1 text-black"/>
-      </a>
-      <a href="https://www.youtube.com"
-         class="ms-3"
-         target=”_blank”>
-        <b-icon-youtube class="fs-1 text-danger"/>
-      </a>
-    </h1>
+    <div class="description d-flex align-items-center ps-md-5">
+      <div>
+        <h2>Project Title
+          <a href="https://github.com/OleksandrLevinskyi"
+             class="ms-3"
+             target=”_blank”>
+            <b-icon-github class="fs-3 text-black"/>
+          </a>
+          <a href="https://www.youtube.com"
+             class="ms-3"
+             target=”_blank”>
+            <b-icon-youtube class="fs-3 text-danger"/>
+          </a>
+        </h2>
 
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi dicta, maiores necessitatibus quam quos
-      recusandae saepe sequi temporibus veniam vero.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi dicta, maiores necessitatibus quam quos
+          recusandae saepe sequi temporibus veniam vero.</p>
 
-    <div class="d-flex">
-      <span class="badge bg-secondary me-3 fs-5">Tech</span>
-      <span class="badge bg-secondary me-3 fs-5">Tech</span>
-      <span class="badge bg-secondary me-3 fs-5">Tech</span>
+        <div class="d-flex">
+          <span class="badge bg-secondary me-3 fs-6">Tech</span>
+          <span class="badge bg-secondary me-3 fs-6">Tech</span>
+          <span class="badge bg-secondary me-3 fs-6">Tech</span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -40,5 +44,21 @@ export default {
 .video {
   aspect-ratio: 16/9;
   width: 100%;
+}
+
+@media (min-width: 768px) {
+  .video {
+    width: 60%;
+  }
+
+  .description {
+    width: 40%;
+  }
+}
+
+@media (min-width: 1200px) {
+  .video, .description {
+    width: 50%;
+  }
 }
 </style>
